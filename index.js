54c8e1425b46e662f0c2b8901a702940afc13e3f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:41:46 +0500

    chore: build konfiguratsiyasi sozlandi

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // kod formatlash va indentatsiya
 // database testlari qo'shildi
 const express = require("express")
+// prettier formatlash
 // component testlari yaratildi
 
 // middleware funksiyalari qo'shildi

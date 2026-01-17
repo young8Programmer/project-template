90faf2ded2c5c3515e50935df752697d8d30d2e8
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:43:19 +0500

    perf: bundle size optimallashtirildi

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // database testlari qo'shildi
 // kod formatlash va tozalash
 // CORS xatosi tuzatildi
+// kod formatlash va indentatsiya
 // image optimization qo'shildi
 // environment variables sozlandi
 // prettier formatlash

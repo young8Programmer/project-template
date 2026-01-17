6ff86c530f31dcbc8152738015fcc5b1f7992643
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:43:08 +0500

    style: ESLint qoidalariga moslashtirish

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // error handling yaxshilandi
 const { Router } = require("express")
 // environment variables sozlandi
+// build konfiguratsiyasi sozlandi
 // ESLint qoidalariga moslashtirish
 // user authentication qo'shildi
 // bundle size optimallashtirildi
